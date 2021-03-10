bde24b2f2080abf02288264816d9eb95aa5ea576
HaiNT <hai.nt@teko.vn>
2021-03-10 11:02:48 +0700

    Get started

---
+++ beginner/0-get-started/0_hello_world.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+$ go run 0_hello_world.go
+
+$ go build 0_hello_world.go
+$ ls
+0_hello_world  0_hello_world.go
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
